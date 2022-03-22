13772fbd1938d77b5ff161f87f82d0dd901a1e4b
ChiaYi-LIN <b04704016@ntu.edu.tw>
2022-03-22 15:24:33 +0800

    export for code submission

---
+++ HW3/export.sh
@@ -0,0 +1,8 @@
+rm -rf ./r10922124_hw3
+mkdir ./r10922124_hw3
+cp ./2022ml_hw3_image_classification.py ./r10922124_hw3
+cp ./test_ensemble.py ./r10922124_hw3
+cp ./README.md ./r10922124_hw3
+rm -f r10922124_hw3.zip
+zip -r r10922124_hw3.zip ./r10922124_hw3
+rm -r ./r10922124_hw3
