70dd2e5f837229c217909d2aa12d372c1dbd966f
ChiaYi-LIN <b04704016@ntu.edu.tw>
2022-03-29 01:34:52 +0800

    export

---
+++ HW4/README.md
@@ -0,0 +1,11 @@
+# Training
+train three models seperatly with random seed 326, 1121 and 1121326
+```python
+python hw04.py
+```
+
+# Predicting
+ensemble three models to get final predictions
+```python
+python test_ensemble.py
+```
---
+++ HW4/export.sh
@@ -0,0 +1,10 @@
+rm -f ./r10922124_hw4
+mkdir -p ./r10922124_hw4
+cp ./hw04.py ./r10922124_hw4
+cp ./test_ensemble.py ./r10922124_hw4
+cp ./README.md ./r10922124_hw4
+
+rm -f r10922124_hw4.zip
+zip -r r10922124_hw4.zip ./r10922124_hw4
+
+rm -r ./r10922124_hw4
