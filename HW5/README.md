bb7e022378bdeccb3280be8a318335d722911161
ChiaYi-LIN <b04704016@ntu.edu.tw>
2022-04-07 16:23:00 +0800

    submit code

---
+++ HW5/README.md
@@ -0,0 +1,7 @@
+# Code
+```python
+python hw05.py
+```
+
+# Report
+r10922124_hw5_report.pdf
---
+++ HW5/export.sh
@@ -0,0 +1,10 @@
+rm -rf ./r10922124_hw5
+mkdir -p ./r10922124_hw5
+cp ./hw05.py ./r10922124_hw5
+cp ./r10922124_hw5_report.pdf ./r10922124_hw5
+cp ./README.md ./r10922124_hw5
+
+rm -f ./r10922124_hw5.zip
+zip -r ./r10922124_hw5.zip ./r10922124_hw5
+
+rm -rf ./r10922124_hw5
