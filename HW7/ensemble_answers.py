bc70d481ed1dba1e5b88155fdde828a5afac3ff3
ChiaYi-LIN <b04704016@ntu.edu.tw>
2022-04-19 23:37:44 +0800

    ensembler

---
+++ HW7/ensemble_answers.py
@@ -0,0 +1,30 @@
+#%%
+import pandas as pd
+output_name = 'ensemble'
+
+#%%
+csv_names = ['roberta_wwm.csv', 'roberta_wwm_1e-4.csv', 'roberta_wwm_2e-4.csv', 'roberta_wwm_9e-5.csv']
+for i, name in enumerate(csv_names):
+    if i == 0:
+        res = pd.read_csv(name)
+        res["Answer_1"] = res["Answer"]
+    else:
+        res = pd.merge(
+            left=res,
+            right=pd.read_csv(name),
+            on='ID',
+            how='left',
+            suffixes=("", f"_{i+1}")
+        )
+    
+#%%
+output = res.drop(["ID", "Answer"], axis=1).mode(axis=1)[[0]]
+output["ID"] = output.index
+
+#%%
+output.set_axis(['Answer', 'ID'], axis=1, inplace=True)
+
+#%%
+output[["ID", "Answer"]].to_csv(f'./{output_name}.csv', index=False)
+
+#%%
